0a1242db399c713ca46797d485b8bbfe3694e625
Fernando Corrêa de Oliveira <fernando.correa@humanstate.com>
2024-09-24 20:27:46 +0100

    feat: Many things

--- lua/bujo/init.lua
+++ lua/bujo/init.lua
@@ -1,48 +1,95 @@
 local M = {}
 
-function curr_date_file(opts)
-	return vim.fn.resolve(vim.fn.expand(os.date(opts.path .. "/%Y-%m-%d.bujo")))
+function M.get_symbol(line)
+	local _, symbol = string.match(line, M.regex)
+	return symbol
 end
 
-function M.setup(opts)
-	opts = vim.tbl_deep_extend("force", {
-		path = "~/bujo",
-		default_symbol = "-",
-		create_task_keymap = "<leader>bc",
-		open_today_keymap = "<leader>bt",
-	}, opts or {})
+function M.date_file(days)
+	return vim.fn.resolve(
+		vim.fn.expand(os.date(M.opts.path .. "/%Y-%m-%d.bujo", os.time() + ((days or 0) * 24 * 60 * 60)))
+	)
+end
 
-	vim.fn.mkdir(vim.fn.expand(opts.path), "p")
+M.symbol_chain = {
+	["-"] = "->",
+	["->"] = "<-",
+	["<-"] = "(-)",
+	["(-)"] = "+",
+	["+"] = "-",
+}
+
+M.reverse_chain = {}
+for key, val in pairs(M.symbol_chain) do
+	M.reverse_chain[val] = key
+end
+
+function M.replace_symbol(reverse)
+	local symbol_chain = M.symbol_chain
+	if reverse then
+		symbol_chain = M.reverse_chain
+	end
+	local indent, symbol, task = string.match(vim.api.nvim_get_current_line(), M.regex)
+	symbol = string.match(symbol, "^%s*(.+)%s*$")
+	if symbol == nil or symbol_chain[symbol] == nil then
+		return
+	end
+	symbol = symbol_chain[symbol]
+	if string.find(symbol, "[-+]") == 1 then
+		symbol = " " .. symbol
+	end
+	vim.api.nvim_set_current_line(indent .. symbol .. "\t" .. task)
+end
+
+M.default_opts = {
+	path = "~/bujo",
+	default_symbol = "-",
+	create_task_keymap = "<leader>bc",
+	open_today_keymap = "<leader>bt",
+	cycle_statuses_keymap = " ",
+	cycle_back_statuses_keymap = "<S- >",
+	create_task_inside_keymap = "bc",
+}
+
+function M.setup(opts)
+	opts = vim.tbl_deep_extend("force", M.default_opts, opts or {})
+
+	M.opts = opts
+	M.regex = "^(%s*)([%s<(][-+][>)]?)%s*(.*)$"
+
+	vim.fn.mkdir(vim.fn.expand(M.opts.path), "p")
 
 	vim.api.nvim_set_keymap(
 		"n",
-		opts.create_task_keymap,
+		M.opts.create_task_keymap,
 		":BujoCreateEntry<CR>",
 		{ silent = true, noremap = true, desc = "Create Bujo task" }
 	)
 
 	vim.api.nvim_set_keymap(
 		"n",
-		opts.open_today_keymap,
+		M.opts.open_today_keymap,
 		":BujoOpenTodaysFile<CR>",
 		{ silent = true, noremap = true, desc = "Open today's file" }
 	)
 
 	vim.api.nvim_create_user_command("BujoOpenTodaysFile", function()
-		local curr_date_file = curr_date_file(opts)
+		local curr_date_file = M.date_file()
 		vim.cmd.edit(curr_date_file)
 	end, { bang = true })
 
 	vim.api.nvim_create_user_command("BujoCreateEntry", function()
 		vim.ui.input({ prompt = "entry a new task:" }, function(task)
-			local regex = "^%s*([<(]?[-+][>)]?)%s*(.*)$"
+			if not task then
+				return
+			end
 
-			local symbol = opts.default_symbol
-			local match = { string.find(task, regex) }
+			local symbol = M.opts.default_symbol
+			local match = { string.match(task, M.regex) }
 			if #match > 0 then
-				task = match[4]
-				if match[3] then
-					symbol = match[3]
+				task = match[3]
+				if match[2] then
+					symbol = match[2]
 				end
 			end
 			if string.find(symbol, "^[-+]") == 1 then
@@ -54,14 +101,20 @@ function M.setup(opts)
 				file = vim.api.nvim_buf_get_name(0),
 			}
 
-			local curr_date_file = curr_date_file(opts)
-			print(curr_date_file .. ": " .. task .. "\n")
+			local curr_date_file = M.date_file()
 			local file = io.open(curr_date_file, "a+")
 			if file == nil then
 				error("Could not open file")
 			end
-			file:write(symbol .. "\t" .. task .. " " .. vim.json.encode(meta) .. "\n")
+			file:write(symbol .. "\t" .. task .. "\t" .. vim.json.encode(meta) .. "\n")
 			io.close(file)
+			if vim.fn.resolve(curr_date_file) == vim.fn.resolve(vim.api.nvim_buf_get_name(0)) then
+				if vim.bo.modified then
+					vim.notify("Buffer modified, not updating...", "error")
+					return
+				end
+				vim.cmd.edit()
+			end
 		end)
 	end, { bang = true })
 end
--- syntax/bujo.lua
+++ syntax/bujo.lua
@@ -11,5 +11,43 @@ vim.api.nvim_set_hl(0, "BujoValue", { bold = true, fg = "grey" })
 vim.cmd.syntax("match", "BujoValue", "/\\s\\+[^{]*/ contained nextgroup=BujoDetails")
 vim.cmd.syntax("conceal", "on")
 vim.api.nvim_set_hl(0, "BujoDetails", { bold = true, fg = "grey" })
-vim.cmd.syntax("match", "BujoDetails", "/{.*}/ contained")
+vim.cmd.syntax("match", "BujoDetails", "/\\s*{.*}/ contained")
 vim.cmd.syntax("conceal", "off")
+
+local bujo = require("bujo")
+
+vim.api.nvim_buf_create_user_command(0, "BujoCurrentLineSymbol", function()
+	print(bujo.get_symbol(vim.api.nvim_get_current_line()))
+end, { bang = true })
+
+vim.api.nvim_buf_create_user_command(0, "BujoNextSymbol", function()
+	bujo.replace_symbol()
+end, { bang = true })
+
+vim.api.nvim_buf_create_user_command(0, "BujoPreviousSymbol", function()
+	bujo.replace_symbol(true)
+end, { bang = true })
+
+vim.api.nvim_buf_set_keymap(
+	0,
+	"n",
+	bujo.opts.cycle_statuses_keymap,
+	":BujoNextSymbol<CR>",
+	{ silent = true, noremap = true, desc = "Rotate Bujo symbols" }
+)
+
+vim.api.nvim_buf_set_keymap(
+	0,
+	"n",
+	bujo.opts.cycle_back_statuses_keymap,
+	":BujoPreviousSymbol<CR>",
+	{ silent = true, noremap = true, desc = "Rotate back Bujo symbols" }
+)
+
+vim.api.nvim_buf_set_keymap(
+	0,
+	"n",
+	bujo.opts.create_task_inside_keymap,
+	":BujoCreateEntry<CR>",
+	{ silent = true, noremap = true, desc = "Create Bujo task" }
+)
